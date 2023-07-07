8195006c9042fc302986c752ca102f45db577217
Владимир Войтенко <vladimirdev635@gmail.com>
2023-07-07 18:33:42 +0500

    remove-glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+#define print(x) std::cout << x << std::endl
+
+#define WINDOW_HEIGHT 640
+#define WINDOW_WIDTH 480
+
 int main() {
     if (!glfwInit())
     {
-        std::cout << "GLFW Initialization failed" << std::endl;
+        print("GLFW Initialization failed");
+        return 1;
     }
-    std::cout << "Hello world!" << std::endl;
+    GLFWwindow* window = glfwCreateWindow(WINDOW_HEIGHT, WINDOW_WIDTH, "My Title", NULL, NULL);
+    if (window == NULL) {
+        print("Window creation failed");
+        glfwTerminate();
+        return 1;
+    }
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
